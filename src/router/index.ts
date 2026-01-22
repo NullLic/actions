66ef691dea4102bdfb04e4542c9566cdb8b98832
nullicn <3469986203@qq.com>
2026-01-22 20:34:12 +0800

    再次尝试

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/actions/',
       name: 'home',
       component: () => import('../pages/ThreePage.vue'),
     },
